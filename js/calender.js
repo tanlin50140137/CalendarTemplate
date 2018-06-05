9c4edee9adcd4e44c76de4649da40b822a8cf8dc
tanlin50140137 <39691304+tanlin50140137@users.noreply.github.com>
2018-06-05 12:56:52 +0800

    calender
    日历模板

--- js/calender.js
+++ js/calender.js
@@ -1,3 +1,24 @@
 /**
  * js
  */
+$(function(){
+	var myDate = new Date();
+	//设置年
+	var yyyy = $("#yyyy");
+	var miny = parseInt(myDate.getFullYear())-118;
+	var maxy = parseInt(myDate.getFullYear())+32;
+	var yhtml = '';
+	yyyy.empty();
+	for(var y=miny;y>=miny&&y<=maxy;y++)
+	{
+		if( y == myDate.getFullYear() )
+		{	
+			yhtml += '<option value="'+y+'" selected="selected">'+y+'年</option>';
+		}
+		else
+		{
+			yhtml += '<option value="'+y+'">'+y+'年</option>';
+		}	
+	}	
+	yyyy.append(yhtml);
+});
